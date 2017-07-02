9fc170e24ba32917b9b17129026dc0638374d3ae
Guba Mikhail <gb.mikhail@gmail.com>
2017-07-02 15:47:10 +0300

    update_2

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ let myModule = {
         this.getFriends();
         this.setListeners();
         this.search();
+        this.saveButton();
     },
-    friends: undefined,
+    friendsIdArr: [],
+    rightFriendsArr: [],
     renderFriend: function (friend) {
         console.log('renderFriend 1');
         let friendDiv = document.createElement('div');
@@ -22,9 +24,31 @@ let myModule = {
         plusButton.innerHTML = '+';
         friendDiv.setAttribute('id', friend.id);
         friendDiv.setAttribute('draggable', 'true');
-        friendDiv.setAttribute('ondragstart', 'dragStart');
+        friendDiv.setAttribute('ondragstart', 'return dragStart(event)');
 
         friends.appendChild(friendDiv);
+        friendDiv.appendChild(friendPhoto);
+        friendDiv.appendChild(friendName);
+        friendDiv.appendChild(plusButton);
+    },
+    renderFriend2: function (friend) {
+        let friendDiv = document.createElement('div');
+        let friendPhoto = new Image();
+        let friendName = document.createElement('div');
+        let plusButton = document.createElement('a');
+
+        friendDiv.classList.add('central-box__friend');
+        friendPhoto.src = friend.photo_50;
+        friendPhoto.classList.add('central-box__friend_img');
+        friendName.classList.add('central-box__friend_name');
+        friendName.textContent = `${friend.first_name} ${friend.last_name}`;
+        plusButton.classList.add('central-box__friend_button');
+        plusButton.innerHTML = '-';
+        friendDiv.setAttribute('id', friend.id);
+        friendDiv.setAttribute('draggable', 'true');
+        friendDiv.setAttribute('ondragstart', 'return dragStart(event)');
+
+        friendsNewlist.appendChild(friendDiv);
         friendDiv.appendChild(friendPhoto);
         friendDiv.appendChild(friendName);
         friendDiv.appendChild(plusButton);
@@ -46,50 +70,81 @@ let myModule = {
         console.log('getFriends 3');
         let self = this;
         return new Promise(function (resolve, reject) {
-            VK.api('friends.get', {v: '5.64', order: 'name', fields: 'photo_50'}, function (response) {
-                if (response.error) {
-                    reject(new Error(response.error.error_msg));
-                } else {
-                    resolve(response);
-                }
-            })
+            //условие в котором мы проводим проверку из локал сторедж, и либо берем друзей оттуда, либо запускаем АPI
+                VK.api('friends.get', {v: '5.64', order: 'name', fields: 'photo_50'}, function (response) {
+                    if (response.error) {
+                        reject(new Error(response.error.error_msg));
+                    } else {
+                        resolve(response);
+                    }
+                })
         })
             .then(function (response) {
-                self.friends = response.response.items;
-                response.response.items.forEach(friend => {
-                    self.renderFriend(friend);
-                });
-            self.dnd();
+                if (localStorage.length == 0) {
+                    response.response.items.forEach(friend => {
+                        self.renderFriend(friend);
+                        self.friendsIdArr.push(friend);
+                    });
+                } else {
+                    console.log('localStor');
+                    localStorage.getItem('local');
+                    localStorage.getItem('local2')
+                }
             })
 
     },
     setListeners: function () {
+        let self = this;
         console.log('setListeners 4');
+
         let button = document.getElementById('friends');
         let button2 = document.getElementById('friendsNewlist');
+        let rightSearch = document.getElementById('right-search-box_input');
+        let leftSearch = document.getElementById('left-search-box_input');
 
         button.addEventListener('click', handler1);
         button2.addEventListener('click', handler1);
 
         function handler1(e) {
+
             if (e.target.innerHTML === '+') {
                 let newTarget = e.target.parentNode;
-                friendsNewlist.appendChild(newTarget);
+                button2.appendChild(newTarget);
                 e.target.innerHTML = '-';
+
+                for (let i = 0; i < self.friendsIdArr.length; i++) {
+                    if (self.friendsIdArr[i].id == newTarget.id) {
+                        self.rightFriendsArr.push(self.friendsIdArr[i]);
+                        self.friendsIdArr.splice(i, 1);
+                        // console.log(self.rightFriendsArr, 'куда');
+                        // console.log(self.friendsIdArr, 'откуда');
+                    }
+                }
+
             } else if (e.target.innerHTML === '-') {
                 let newTarget = e.target.parentNode;
-                friends.appendChild(newTarget);
+                button.appendChild(newTarget);
                 e.target.innerHTML = '+';
+                for (let i = 0; i < self.rightFriendsArr.length; i++) {
+                    if (self.rightFriendsArr[i].id == newTarget.id) {
+                        self.friendsIdArr.push(self.rightFriendsArr[i]);
+                        self.rightFriendsArr.splice(i, 1);
+                        // console.log(self.rightFriendsArr, 'откуда');
+                        // console.log(self.friendsIdArr, 'куда');
+                    }
+                }
+
+
             }
         }
     },
     search: function () {
         console.log('search 5');
         let leftSearchInput = document.getElementById('left-search-box_input');
-        let leftFriendsList = document.getElementById('friends');
+        let rightSearchInput = document.getElementById('right-search-box_input');
 
         function clearList() {
-
+            let leftFriendsList = document.getElementById('friends');
             for (let curNode = leftFriendsList.firstChild; curNode != null;) {
                 let nextNode = curNode.nextSibling;
 
@@ -98,51 +153,54 @@ let myModule = {
             }
         }
 
+        function clearList2() {
+            let rightFriendsList = document.getElementById('friendsNewlist');
+            for (let curNode = rightFriendsList.firstChild; curNode != null;) {
+                let nextNode = curNode.nextSibling;
+
+                rightFriendsList.removeChild(curNode);
+                curNode = nextNode;
+            }
+        }
+
         leftSearchInput.addEventListener('keyup', someName.bind(this));
+        rightSearchInput.addEventListener('keyup', someName2.bind(this));
 
         function someName(e) {
             let toVal = e.target.value;
-            let someArr = [];
             clearList();
-            for (var i = 0; i < this.friends.length; i++) {
-                this.friends[i].fullName = this.friends[i].first_name + ' ' + this.friends[i].last_name;
-                if (this.friends[i].fullName.toLowerCase().startsWith(toVal)) {
-                    this.renderFriend(this.friends[i]);
+            for (let i = 0; i < this.friendsIdArr.length; i++) {
+                this.friendsIdArr[i].fullName = this.friendsIdArr[i].first_name + ' ' + this.friendsIdArr[i].last_name;
+                if (this.friendsIdArr[i].fullName.toLowerCase().startsWith(toVal)) {
+                    this.renderFriend(this.friendsIdArr[i]);
+                }
+            }
+        }
+
+        function someName2(e) {
+            let toVal = e.target.value;
+            clearList2();
+            for (let i = 0; i < this.rightFriendsArr.length; i++) {
+                this.rightFriendsArr[i].fullName2 = this.rightFriendsArr[i].first_name + ' ' + this.rightFriendsArr[i].last_name;
+                if (this.rightFriendsArr[i].fullName2.toLowerCase().startsWith(toVal)) {
+                    this.renderFriend2(this.rightFriendsArr[i]);
                 }
             }
         }
     },
-    dnd: function () {
-        console.log('dnd 6');
+    saveButton: function () {
+        let  self = this;
 
-        function dragStart(ev) {
-            ev.dataTransfer.effectAllowed='move';
-            ev.dataTransfer.setData("Text", ev.target.getAttribute('id'));
-            ev.dataTransfer.setDragImage(ev.target,100,100);
-            return true;
-        }
+        let button = document.querySelector('.friend-box__bottom-box_button');
 
-        function dragEnter(ev) {
-            ev.preventDefault();
-            console.log('dragEnter');
-            return true;
-
-        }
-
-        function dragOver(ev) {
-            ev.preventDefault();
-        }
-
-        function dragDrop(ev) {
-            var data = ev.dataTransfer.getData("Text");
-            ev.target.appendChild(document.getElementById(data));
-            ev.stopPropagation();
-            return false;
-        }
-    },
+        button.addEventListener('click', function (e) {
+            console.log('hellog');
+            localStorage.setItem('local', self.friendsIdArr);
+            localStorage.setItem('local2', self.rightFriendsArr);
+            // localStorage.clear();
+        })
+    }
 };
-
-
 
 window.onload = myModule.init();
 
